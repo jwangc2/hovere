593b3397856eca2d6b35a7305a4af16ff386186e
Joshua Wang <jwangc2@gmail.com>
2014-11-24 21:56:54 -0500

    Imported MoCap animations and Created Basic Animation Machine

---
+++ Assets/Scripts/CamPOV.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamPOV : MonoBehaviour {
+
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		Update ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target)
+		{
+			this.transform.position = target.position;
+			this.transform.rotation = target.rotation;
+		}
+	}
+}
---
+++ Assets/Scripts/InputWalk.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputWalk : MonoBehaviour {
+
+	Animator animator;
+	float walk = 0.0f;
+	float sprint = 0.0f;
+	float dir = 0.0f;
+	// Use this for initialization
+	void Start () {
+		animator = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Get the inputs
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		bool sp = Input.GetButton("Fire1");
+
+		// Determine the speeds
+		walk = v * v;
+		sprint = 0f;
+		if (sp)
+		{
+			sprint = v * v;
+		}
+
+		// Turning
+		dir = dir + (h - dir) * 0.1f;
+	}
+
+	void FixedUpdate() {
+		if (animator)
+		{
+			animator.SetFloat("Walk", walk);
+			animator.SetFloat("Sprint", sprint);
+			animator.SetFloat("Turn", dir);
+		}
+	}
+}
